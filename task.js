9b31763066d44ee995d9dd1e3408cabbc1617a8c
K.Durga Bhavani <bhavanidurga759@gmail.com>
2024-09-19 22:47:47 +0530

    Add files via upload

---
+++ task.js
@@ -0,0 +1,25 @@
+var a;    //declaration
+a="bhavani";     //assignig
+var a;     //declaration
+a="divya";     //assignig
+console.log(a)   //consoling
+
+var a="nanna";  //intialization
+var a="amma";  //re-intialization 
+console.log(a);   //consoling
+
+let b;    //declaration     let is a cannot declare the same variable with let in the same scope. This helps in avoiding unintentional overwriting of variables.
+b=100;    //assigning
+// let b;    //re-declaration
+b=40;     //re-assigning
+b=500;    //re-assigning
+let c="chinna";  //intialization
+// lat c="bhavani";  //re-initialization
+console.log(b);  //consoling
+
+// const RRR;  //re-declaration  and const is a cannot re-declare a variable in the same scope
+// const RRR;    //re-declaration
+
+
+
+
